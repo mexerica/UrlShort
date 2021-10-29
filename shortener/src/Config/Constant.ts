930a997a902ac3616431bfd9b95d89ee19dcfcef
tiago <82677798+mexerica@users.noreply.github.com>
2021-10-29 15:27:51 -0300

    Add files via upload

---
+++ shortener/src/Config/Constant.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://Tiagueira:<e8YRqqYFq5bbxAO>@cluster0.jo1s3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
